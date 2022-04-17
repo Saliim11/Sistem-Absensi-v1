726f5a2e6d4aba337bb598bf779e9317fef927d4
rizkyr117h <84673678+rizkyr117h@users.noreply.github.com>
2022-04-17 17:25:51 +0700

    Penambahan methode Generic

--- Sistem Absensi v1/Form1.Designer.cs
+++ Sistem Absensi v1/Form1.Designer.cs
@@ -28,17 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Login = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.input_Password = new System.Windows.Forms.TextBox();
+            this.input_Username = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.Login.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Login.Location = new System.Drawing.Point(364, 310);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(120, 27);
+            this.Login.TabIndex = 11;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(373, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Please Input Password";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(373, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Please Input Username";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(361, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 8;
+            // 
+            // input_Password
+            // 
+            this.input_Password.Location = new System.Drawing.Point(290, 219);
+            this.input_Password.Multiline = true;
+            this.input_Password.Name = "input_Password";
+            this.input_Password.Size = new System.Drawing.Size(266, 43);
+            this.input_Password.TabIndex = 7;
+            this.input_Password.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // input_Username
+            // 
+            this.input_Username.Location = new System.Drawing.Point(290, 121);
+            this.input_Username.Multiline = true;
+            this.input_Username.Name = "input_Username";
+            this.input_Username.Size = new System.Drawing.Size(266, 43);
+            this.input_Username.TabIndex = 6;
+            this.input_Username.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(846, 412);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.input_Password);
+            this.Controls.Add(this.input_Username);
             this.Name = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox input_Password;
+        private System.Windows.Forms.TextBox input_Username;
     }
 }
 
--- Sistem Absensi v1/Form1.cs
+++ Sistem Absensi v1/Form1.cs
@@ -12,11 +12,39 @@ namespace Sistem_Absensi_v1
 {
     public partial class Form1 : Form
     {
+        string username;
+        string password;
 
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            username = input_Username.Text;
+            password = input_Password.Text;
+            User.Login<String>(username, password);
+        }
     }
 }
