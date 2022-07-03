b5433771862bf9280104c12811769e968aa86516
Saliim11 <qalbunsaliimbakhri@gmail.com>
2022-07-03 23:08:39 +0700

    menambahkan class testing Saliim dan menambahkan library tambahan

---
+++ UnitTestSaliim/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestSaliim
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
