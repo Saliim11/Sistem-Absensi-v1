0e113561182f8e8ed2614dab4cc449b0acb1d03e
RAFI ALIF <rafialif2907@gmail.com>
2022-07-03 10:53:27 +0700

    menambahkan class siswa get & set

---
+++ Sistem Absensi API/Siswa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace sistem_absensi
+{
+    public class Siswa
+    {
+        public DateTime date { get; set; }
+        public string nama { get; set; }
+        public string nim { get; set; }
+        public string kelas { get; set; }
+        public Siswa(DateTime date,string nama, string nim, string kelas)
+        { 
+            this.date = DateTime.Now;
+            this.nama = nama;
+            this.nim = nim;
+            this.kelas = kelas; 
+        }
+    }
+}
